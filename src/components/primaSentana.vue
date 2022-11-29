dadb5748155f432afadcdb6be443f6364ed1f489
Prima Sentana <premasentana123@gmail.com>
2022-11-29 11:16:48 +0800

    Initial Commit

---
+++ src/components/primaSentana.vue
@@ -0,0 +1,26 @@
+<script lang="js">
+
+export default {
+    data(){
+        return {
+            clicked : 0
+        }
+    },
+    methods : {
+        increment: function() {
+            this.clicked++;
+        }
+    }
+}
+
+</script>
+
+<template>
+    <p>Value = {{clicked}} !</p>
+    <button @click="increment" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Click Here !</button>
+</template>
+
+<style>
+
+</style>
+
